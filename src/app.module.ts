ef305d84ee478f3638cb99d8e281d6aab705637b
엘래 <elle0510@gmail.com>
2021-09-05 12:09:16 +0900

    auth 모듈 전체적인 기본틀 잡기

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeORMConfig } from './configs/typeorm.config';
 import { BoardModule } from './board/board.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
