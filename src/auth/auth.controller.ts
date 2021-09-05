e2509debb818bebe963b5a54ecca9a1c78db514a
엘래 <elle0510@gmail.com>
2021-09-05 17:35:34 +0900

    사용자 등록 signup 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthCredentialDto } from './dtos/auth-credential.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto): Promise<void> {
+    return this.authService.signUp(authCredentialDto);
+  }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,13 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialDto } from './dtos/auth-credential.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = await this.create({ username, password });
+
+    await this.save(user);
+  }
+}
