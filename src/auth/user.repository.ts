981b32291f648302a238f75961b3471d0c1ebb9c
엘래 <elle0510@gmail.com>
2021-09-05 17:45:00 +0900

    unique 한 유저이름 등록

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialDto } from './dtos/auth-credential.dto';
 import { User } from './user.entity';
@@ -8,6 +9,14 @@ export class UserRepository extends Repository<User> {
     const { username, password } = authCredentialDto;
     const user = await this.create({ username, password });
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
